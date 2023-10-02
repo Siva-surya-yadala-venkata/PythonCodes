a304aaea8f1ac8cb7b78427d3cb9d77956e510ae
SivaSurya007 <122281821+Siva-surya-yadala-venkata@users.noreply.github.com>
2023-10-02 18:08:40 +0530

    Add files via upload
    THIS IS DONE BY YV SIVA SURYA LEARNING PYTHON FROM DIFFERENT RESOURCES
                                                                                                                                                         ~YV SIVA SURYA

---
+++ ifElsifElse.py
@@ -0,0 +1,14 @@
+#If , Elif , else
+
+#Program to find out the number is even or not
+
+number=int(input("Enter the Number Whether is Even Or not to find out"))
+
+print("The Number You Entered is",number,sep=" ")
+
+if(number % 2 ==0):
+    print("you entered ",number," is Even")
+else:
+    print("you entered number ",number," is odd")
+
+print("Thank You!") 
---
+++ positiveOrNotOrZero.py
@@ -0,0 +1,27 @@
+#Finding Out the NUmber whether it is positive or not
+#We are Using 2 Methods 
+
+#Method 1
+number=int(input("Enter The Number Whether Positive,Negative,Or Zero"))
+
+if(number>0):
+    print("The Number You entered is",number,"is Positive")
+elif(number<0):
+    print("The Number You Enterd is",number,"is Negative")
+else:
+    print("The Number You entered is",number,"zero")
+print("This is Method 1")
+
+#Method 2
+if(number>0):
+    print("The Number You entered is",number,"is Positive")
+if(number<0):
+    print("The Number You entered is",number,"is Negative")
+if(number==0):
+    print("The Number You entered is",number,"is Zero")
+print("This is Method 2")
+
+
+    
+
+    
