16e4321556cac7338bf572e65b0614cf34ce5302
SivaSurya007 <122281821+Siva-surya-yadala-venkata@users.noreply.github.com>
2023-10-02 18:26:45 +0530

    positiveOrNotOrZero.py

--- positiveOrNotOrZero.py
+++ positiveOrNotOrZero.py
@@ -4,20 +4,20 @@
 #Method 1
 number=int(input("Enter The Number Whether Positive,Negative,Or Zero"))
 
-if(number>0):
+if number>0:
     print("The Number You entered is",number,"is Positive")
-elif(number<0):
+elif number<0:
     print("The Number You Enterd is",number,"is Negative")
 else:
     print("The Number You entered is",number,"zero")
 print("This is Method 1")
 
 #Method 2
-if(number>0):
+if number>0:
     print("The Number You entered is",number,"is Positive")
-if(number<0):
+if number<0:
     print("The Number You entered is",number,"is Negative")
-if(number==0):
+if number==0:
     print("The Number You entered is",number,"is Zero")
 print("This is Method 2")
 
